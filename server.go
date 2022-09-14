0feabc386daea2b3dd8809d5104858064f3389d8
OmarTariq612 <az2339913@gmail.com>
2022-09-14 15:20:19 +0200

    colorful logs + http proxy fix (non-connect methods)

--- server.go
+++ server.go
@@ -18,14 +18,22 @@ func NewFroxyProxy(address string, allowedPorts []string) *FroxyProxy {
 	return &FroxyProxy{address: address, allowedPorts: allowedPorts}
 }
 
+const (
+	green = "\033[92m"
+	// blue   = "\033[94m"
+	// red    = "\033[0;31m"
+	orange = "\033[38;5;214m"
+	end    = "\033[0m"
+)
+
 func (s *FroxyProxy) ListenAndServe() error {
 	log.Println("Serving on", s.address)
 	log.Println("CONNECT allowed ports:", s.allowedPorts)
 
 	return http.ListenAndServe(s.address, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("[%v]: %v - from %v\n", r.Method, r.Host, r.RemoteAddr)
 		switch r.Method {
 		default:
+			log.Printf("%s[%s]%s: %s - from %s\n", orange, r.Method, end, r.Host, r.RemoteAddr)
 			resp, err := http.DefaultTransport.RoundTrip(r)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusServiceUnavailable)
@@ -39,6 +47,8 @@ func (s *FroxyProxy) ListenAndServe() error {
 					w.Header().Add(key, value)
 				}
 			}
+			w.WriteHeader(resp.StatusCode)
+
 			// copy body
 			_, err = io.Copy(w, resp.Body)
 			if err != nil {
@@ -46,6 +56,7 @@ func (s *FroxyProxy) ListenAndServe() error {
 			}
 
 		case http.MethodConnect:
+			log.Printf("%s[%s]%s: %s - from %s\n", green, r.Method, end, r.Host, r.RemoteAddr)
 			io.Copy(io.Discard, r.Body)
 			_, portStr, _ := net.SplitHostPort(r.Host)
 			for _, port := range s.allowedPorts {
@@ -66,12 +77,14 @@ func (s *FroxyProxy) ListenAndServe() error {
 					clientConn, _, err := hj.Hijack()
 					if err != nil {
 						http.Error(w, err.Error(), http.StatusServiceUnavailable)
+						return
 					}
 					defer clientConn.Close()
 					// w.WriteHeader(http.StatusOK) puts "Transfer-Encoding: chunked" header
 					// and this behaviour can't be avoided
 					// for this reason I'm writing the status code response directly to the socket in this way:
-					clientConn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+					clientConn.Write([]byte("HTTP/1.1 200 OK\r\n"))
+					clientConn.Write([]byte(fmt.Sprintf("Date: %s\r\n\r\n", time.Now().Format(http.TimeFormat))))
 
 					errc := make(chan error, 2)
 					go func() {
@@ -95,7 +108,8 @@ func (s *FroxyProxy) ListenAndServe() error {
 					return
 				}
 			}
-			w.WriteHeader(http.StatusBadRequest)
+
+			http.Error(w, fmt.Sprintf("(%s) port is not allowed", portStr), http.StatusForbidden)
 		}
 	}))
 }
